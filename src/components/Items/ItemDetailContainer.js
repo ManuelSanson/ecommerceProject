64c6351d8966cf22ba4b5a3c346c7361e929d6ba
Manuel Sanson <manu.sanson9@gmail.com>
2022-10-05 13:06:28 -0300

    corrections to display ItemDetail product

--- src/components/Items/ItemDetailContainer.js
+++ src/components/Items/ItemDetailContainer.js
@@ -1,20 +1,34 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
 import { useParams } from "react-router-dom";
+import { getProduct } from "../../utils/products";
+import ItemDetail from "./ItemDetail";
 
 const ItemDetailContainer = () => {
+    
+    const [product, setProduct] = useState(null);
     const { id } = useParams();
-    
+
     useEffect(() => {
-        console.log(id);
+        const fetchData = async () => {
+            try {
+                const response = await getProduct(id);
+                if(response){
+                    setProduct(response)
+                }
+            } catch (error) {
+                console.warn(error);
+            }
+        } 
+        fetchData()
     }, [id])
-    
+
     return (
         <Container>
-            <h1>Detalle del producto</h1>
-            
+            <h1 className="mt-3">Detalle del Producto</h1>
+            {product && <ItemDetail product={product} />}
         </Container>
-    )
+    );
 }
 
 export default ItemDetailContainer;
--- src/utils/products.js
+++ src/utils/products.js
@@ -1,7 +1,7 @@
 const products = [
-    { id: "1", title: "Camiseta", description: "Camiseta de algodón negra", price: 600, pictureURL: "https://res.cloudinary.com/diqry3ihg/image/upload/v1664831392/productPictures/remeraNegra_a7pxfc.png", stock: 80 },
-    { id: "2", title: "Buzo", description: "Buzo medio cierre negro", price: 800, pictureURL: "https://res.cloudinary.com/diqry3ihg/image/upload/v1664831372/productPictures/buzoMedioCierre_h65mis.png", stock: 20 },
-    { id: "3", title: "Campera", description: "Campera negra sin capucha", price: 1400, pictureURL: "https://res.cloudinary.com/diqry3ihg/image/upload/v1664831385/productPictures/campera_nzzcoz.png", stock: 12 }
+    { id: 1, title: "Camiseta", description: "Camiseta de algodón negra", price: 600, pictureURL: "https://res.cloudinary.com/diqry3ihg/image/upload/v1664831392/productPictures/remeraNegra_a7pxfc.png", stock: 80 },
+    { id: 2, title: "Buzo", description: "Buzo medio cierre negro", price: 800, pictureURL: "https://res.cloudinary.com/diqry3ihg/image/upload/v1664831372/productPictures/buzoMedioCierre_h65mis.png", stock: 20 },
+    { id: 3, title: "Campera", description: "Campera negra sin capucha", price: 1400, pictureURL: "https://res.cloudinary.com/diqry3ihg/image/upload/v1664831385/productPictures/campera_nzzcoz.png", stock: 12 }
 ]
 
 export const getProducts = () => {
@@ -12,15 +12,16 @@ export const getProducts = () => {
     })
 
     return promise
-}
+};
 
 export const getProduct = (id) => {
-    const promise = new Promise ((resolve) => {
-        const result = products.find((product) => product.id === id)
+    const promise = new Promise((resolve) => {
+        const result = products.find((product) => product.id === Number(id))
+
         setTimeout(() => {
-            return resolve(result)
+        return resolve(result);
         }, 2000)
     })
 
     return promise
-}
+};
