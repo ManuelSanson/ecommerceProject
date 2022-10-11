a7f81de603c41c1fd230404bd7cc01ae32577053
Manuel Sanson <manu.sanson9@gmail.com>
2022-10-11 10:59:55 -0300

    responsive navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,20 +5,23 @@ import "./NavBar.css";
 
 const NavBar = () => {
     return (
-        <Navbar bg="danger" variant="dark">
+        <Navbar bg="danger" variant="dark" expand="lg">
             <Container>
                 <Navbar.Brand as={Link} to='/' className="storeLogoName">
                     <img src="https://creazilla-store.fra1.digitaloceanspaces.com/emojis/46288/rugby-football-emoji-clipart-md.png" alt="Logo" className="storeLogo"/>
                     <div className="storeName">The Rugby Corner</div>
                 </Navbar.Brand>
-                <Nav className="me-auto">
-                    <Nav.Link as={Link} to='/' className="navLink">Home</Nav.Link>
-                    <Nav.Link as={Link} to='category/jerseys' className="navLink">Jerseys</Nav.Link>
-                    <Nav.Link as={Link} to='category/hoodies' className="navLink">Hoodies</Nav.Link>
-                    <Nav.Link as={Link} to='category/shorts' className="navLink">Shorts</Nav.Link>
-                    <Nav.Link as={Link} to='category/contacto' className="navLink">Contacto</Nav.Link>
-                </Nav>
-                <CartWidget/>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link as={Link} to='/' className="navLink">Home</Nav.Link>
+                        <Nav.Link as={Link} to='category/jerseys' className="navLink">Jerseys</Nav.Link>
+                        <Nav.Link as={Link} to='category/hoodies' className="navLink">Hoodies</Nav.Link>
+                        <Nav.Link as={Link} to='category/shorts' className="navLink">Shorts</Nav.Link>
+                        <Nav.Link as={Link} to='category/contacto' className="navLink">Contacto</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            <CartWidget/>
             </Container>
         </Navbar>
     )
