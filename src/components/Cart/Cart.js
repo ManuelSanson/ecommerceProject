cff5c6f523b07e3ac293ef499e96e7a8f8857a71
Manuel Sanson <manu.sanson9@gmail.com>
2022-10-18 16:14:24 -0300

    showing total

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -6,16 +6,19 @@ import './Cart.css';
 import { Link } from "react-router-dom";
 
 const Cart = () => {
-    const { cartList, removeList } = useCartContext();
+    const { cartList, removeList, calculateTotal } = useCartContext();
 
     return (
         <Container>
-            {cartList.length ? 
+            {cartList.length ?
                 <div>
                     <h1>Shopping Cart</h1>
                     {cartList.map((product) => 
                     <CartView key={product.id} product={product}/>
                     )}
+                    <div className="totalContainer m-4">
+                        <h2> Total: $ {calculateTotal()} </h2>
+                    </div>
                     <div className='cartButtonsContainer'>
                         <Button variant='light'  className='border' as={Link} to='/'> Continue Shopping </Button>
                         <Button variant='danger'  className='emptyCartButton' onClick={() => removeList()}> Empty Cart </Button>
