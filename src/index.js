77bff16fde40ecb19180e4d2aa41874168b39899
Manuel Sanson <manu.sanson9@gmail.com>
2022-10-18 17:27:08 -0300

    get products from firebase

--- src/index.js
+++ src/index.js
@@ -4,6 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAgphzK7VqDfqs-Wdkkm0nmIyMWieQ2x8M",
+  authDomain: "rugby-clothes-ecommerce.firebaseapp.com",
+  projectId: "rugby-clothes-ecommerce",
+  storageBucket: "rugby-clothes-ecommerce.appspot.com",
+  messagingSenderId: "69825231104",
+  appId: "1:69825231104:web:7f296b14ebbb0018971be5"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
--- src/utils/products.js
+++ src/utils/products.js
@@ -1,41 +1,63 @@
-const products = [
+import { getFirestore, collection, getDocs, getDoc, doc, query, where } from 'firebase/firestore';
+
+/*const products = [
     { id: 1, title: "All Blacks Jersey", category: 'jerseys', description: "New Zealand All Blacks Home Rugby Shirt 2021", price: 1600, pictureURL: "https://www.lovellrugby.es/products/products_760x507/905593.jpg", stock: 13 },
     { id: 2, title: "Scotland Hoodie", category: 'hoodies', description: "Scotland Cotton Hoodie", price: 2300, pictureURL: "https://www.lovellrugby.es/products/products_760x507/430063.jpg", stock: 18 },
     { id: 3, title: "England Short", category: 'shorts', description: "England Home Shorts 2021 2022", price: 1100, pictureURL: "https://www.lovellrugby.es/products/products_760x507/955012.jpg", stock: 12 },
     { id: 4, title: "B&I Lions Jersey", category: 'jerseys', description: "British and Irish Lions Pro Shirt Tango Red", price: 1700, pictureURL: "https://www.lovellrugby.es/products/products_760x507/349151.jpg", stock: 18 },
     { id: 5, title: "Italy Hoodie", category: 'hoodies', description: "Italy Full Zip Hoodie", price: 2200, pictureURL: "https://www.lovellrugby.es/products/products_760x507/592813.jpg", stock: 10 },
     { id: 6, title: "Scotland Short", category: 'shorts', description: "Scotland 20/21 Training Shorts", price: 900, pictureURL: "https://www.lovellrugby.es/products/products_760x507/430093.jpg", stock: 20 }
-]
+] */
 
 
 export const getAllProducts = () => {
-    const promise = new Promise ((resolve) => {
-        setTimeout(() => {
-            return resolve(products)
-        }, 2000)
-    })
-
-    return promise
+    const database = getFirestore();
+    const collectionReference = collection(database, 'items')
+    
+    return getDocs(collectionReference)
+        .then((snapshot) => {
+            const list = snapshot
+                .docs
+                .map((doc) => ({
+                    id: doc.id,
+                    ...doc.data()
+                }));
+            return list;
+        })
+        .catch(error => console.warn(error));
 };
 
-export const getProductsByCategory = (categoryName) => {
-    const promise = new Promise((resolve) => {
-        const results = products.filter((product) => product.category === categoryName)
-        setTimeout(() => {
-        return resolve(results);
-        }, 2000)
-    })
+export const getProductsByCategory = (categoryId) => {
+    const database = getFirestore();
+    const collectionReference = collection(database, 'items')
 
-    return promise
+    const collectionQuery = query(collectionReference, where('category', '==', categoryId))
+    return getDocs(collectionQuery)
+        .then((snapshot) => {
+            const list = snapshot
+                .docs
+                .map((doc) => ({
+                    id: doc.id,
+                    ...doc.data()
+                }));
+            return list;
+        })
+        .catch(error => console.warn(error));
 };
 
 export const getProduct = (id) => {
-    const promise = new Promise((resolve) => {
-        const result = products.find((product) => product.id === Number(id))
-        setTimeout(() => {
-        return resolve(result);
-        }, 2000)
-    })
-
-    return promise
+    const database = getFirestore();
+    const itemReference = doc(database, 'items', id);
+    
+    return getDoc(itemReference)
+        .then((snapshot) => {
+            if(snapshot.exists()) {
+                const item = {
+                    id: snapshot.id,
+                    ...snapshot.data()
+                };
+                return item
+            }
+        })
+        .catch(error => console.warn(error));
 };
