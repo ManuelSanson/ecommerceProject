e91abd56c7f55eb36d576ef44b6e2cfe154a6011
Manuel Sanson <manu.sanson9@gmail.com>
2022-10-19 14:21:06 -0300

    cart view and order summary

--- src/components/Cart/CartView.js
+++ src/components/Cart/CartView.js
@@ -4,6 +4,7 @@ import { BsTrash } from 'react-icons/bs'
 import './CartView.css';
 import { useCartContext } from "../../context/CartContext";
 import { useState } from "react";
+import OrderSummary from "./OrderSummary";
 
 const CartView = ({product}) => {
     const { deleteItem } = useCartContext();
@@ -11,19 +12,27 @@ const CartView = ({product}) => {
     const [count, setCount] = useState(product.quantity);
     
     return (
-        <section className="cartItemsContainer">
-            <div>
-                <Card className='cartItemCard' style={{width: "60rem",}}>
-                    <Card.Img variant="top" src={product.pictureURL} className='p-2 cartItemCardPicture'/>
-                    <Card.Body className='cartItemBody'>
-                        <Card.Title> {product.title} </Card.Title>
-                        <Card.Text className='cartItemPrice'>
-                            $ {product.price}
-                        </Card.Text>
-                        <ItemCount product={product} count={count} setCount={setCount}/>
-                        <button className='deleteButton' onClick={() => deleteItem(product.id)}> <BsTrash/> </button>
-                    </Card.Body>
-                </Card>
+        <section className="m-5">
+            <div className="yourCartItems">
+                <h2>Your Cart:</h2>
+                <h4> Items: {product.quantity} </h4>
+            </div>
+            <div className="cartItemsContainer">
+                <div>
+                    <Card className='cartItemCard' style={{width: "50rem",}}>
+                        <Card.Img variant="top" src={product.pictureURL} className='p-2 cartItemCardPicture'/>
+                        <Card.Body className='cartItemBody fw-bold'>
+                            <Card.Title> {product.title} </Card.Title>
+                            <Card.Text className='mb-0 cartItemPrice'>
+                                $ {product.price}
+                            </Card.Text>
+                            <ItemCount product={product} count={count} setCount={setCount}/>
+                            <button className='deleteButton' onClick={() => deleteItem(product.id)}> <BsTrash/> </button>
+                        </Card.Body>
+                    </Card>
+                </div>
+                <OrderSummary/>
+                
             </div>
         </section>
     )
---
+++ src/components/Cart/OrderSummary.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Button, Card } from 'react-bootstrap';
+import { useCartContext } from '../../context/CartContext';
+
+const OrderSummary = () => {
+    const { totalPrice } = useCartContext();
+
+    return (
+        <div className="orderSummary">
+                    <Card className='' style={{width: "18rem",}}>
+                            <Card.Body className=''>
+                                <Card.Title className='mb-4 fs-2 text-center'> Order Summary </Card.Title>
+                                <div className='m-2 orderSummaryDetail'>
+                                    <Card.Text className='fw-bold'>
+                                        Subtotal:
+                                    </Card.Text>
+                                    <Card.Text>
+                                        $ {totalPrice}
+                                    </Card.Text>
+                                </div>
+                                <div className='m-2 orderSummaryDetail'>
+                                    <Card.Text className='fw-bold'>
+                                        Delivery:
+                                    </Card.Text>
+                                    <Card.Text>
+                                        $ 200
+                                    </Card.Text>
+                                </div>
+                                <div className='m-4 orderSummaryDetail fs-4'>
+                                    <Card.Text className='fw-bold'>
+                                        Total:
+                                    </Card.Text>
+                                    <Card.Text>
+                                        $ {totalPrice + 200}
+                                    </Card.Text>
+                                </div>
+                                <div className='text-center mt-5'>
+                                    <Button variant='success' className='border'> Complete purchase </Button>
+                                </div>
+                            </Card.Body>
+                        </Card>
+                    </div>
+    )
+}
+
+export default OrderSummary;
