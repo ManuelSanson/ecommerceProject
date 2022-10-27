8f5c9a5ab5c6d80f6d131267dbd4976fa45d9885
Manuel Sanson <manu.sanson9@gmail.com>
2022-10-27 17:57:26 -0300

    create strongproducts carousel

--- src/components/StrongProducts/StrongProducts.js
+++ src/components/StrongProducts/StrongProducts.js
@@ -1,7 +1,10 @@
-import { useEffect, useState } from "react"
-import { Spinner } from "react-bootstrap"
-import { getStrongProducts } from "../../utils/products"
-import ItemList from "../Items/ItemList"
+import { useEffect, useState } from "react";
+import { Button, Carousel, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { getStrongProducts } from "../../utils/products";
+import Spinner from "../Spinner/Spinner";
+//import ItemList from "../Items/ItemList";
+import './StrongProducts.css';
 
 const StrongProducts = () => {
     
@@ -20,11 +23,53 @@ const StrongProducts = () => {
         } 
         fetchData()
     }, [])
+    console.log(products);
 
     return (
         <div>
-            <h3> You may also like... </h3>
-            {products.length > 0 ? <ItemList products={products}/> : <div className='spinnerContainer'><Spinner/></div>}
+            <h3> You may also like... </h3>            
+            {products.length > 0 ?
+            <Container className='carouselContainer'>
+                <Carousel fade interval={1500} variant='dark'>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src={products[0].pictureURL}
+                        alt="First slide"
+                        />
+                        <Carousel.Caption>
+                        <Button className='mt-1' variant="danger" as={Link} to={`/item/${products[0].id}`}> 
+                            View details
+                        </Button>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src={products[1].pictureURL}
+                        alt="First slide"
+                        />
+                        <Carousel.Caption >
+                        <Button className='mt-1' variant="danger" as={Link} to={`/item/${products[1].id}`}> 
+                            View details
+                        </Button>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src={products[2].pictureURL}
+                        alt="First slide"
+                        />
+                        <Carousel.Caption>
+                        <Button className='mt-1' variant="danger" as={Link} to={`/item/${products[2].id}`}> 
+                            View details
+                        </Button>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
+            </Container>
+            : <div className='spinnerContainer'><Spinner/></div>}
         </div>
     )
 }
